e8040e6bada50080f03d44758124c6f74a4798b1
Abdellah DAMRI <damriabdellah@gmail.com>
2024-07-08 13:33:34 +0100

    feat: first commit

---
+++ backend/src/main/java/org/dgitalbanking/backend/exceptions/CustomerNotFoundException.java
@@ -0,0 +1,7 @@
+package org.dgitalbanking.backend.exceptions;
+
+public class CustomerNotFoundException extends Exception {
+    public CustomerNotFoundException(String message) {
+        super(message);
+    }
+}
